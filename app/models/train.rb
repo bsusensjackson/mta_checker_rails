49adb34de8e1463bdee84065e40fafee46e6c8f7
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2015-02-28 22:55:06 -0500

    refactor train & train_spec

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,36 +1,20 @@
 class Train < ActiveRecord::Base
   has_many :service_outages
-  LINES = ['456', 'ACE']
-  
-  class << self
-    def look_for_service_status!
-      JSONParser.call
-      LINES.each do |line|
-        response = JSONParser.find_status(line)
-        
-        t = Train.find_or_create_by(name: line)
-        t.search(response)
-      end
-    end
-  end
-  
- def search(response)
-    if response['status'] == 'DELAYS'
-      s = ServiceOutage.find_or_create_by(train_id: self.id)
-      if self.outage?
-        #Blah blah 
-      else
-        s.outage_start = Chronic.parse('now')
-        self.set_outage(true)
-      end
-    end 
+  LINES = %w(456, ACE, 123, 7, BDFM, G, JZ, L, NQR, S)
+  attr_reader :current_outage
+  def outage?
+    self.current_outage
   end
 
- def outage?
-   self.current_outage
- end
+  def set_current_outage(status)
+    self.current_outage = status
+  end
 
- def set_outage(status)
-   self.current_outage = status
- end
+  def create_current_outage
+    @current_outage = ServiceOutage.create(train_id: self.id, outage_start: Chronic.parse('now'))
+  end
+
+  def end_current_outage
+    @current_outage.outage_end = Chronic.parse('now')
+  end
 end
--- spec/models/train_spec.rb
+++ spec/models/train_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Train, :type => :model do
-  it "creates a service outage when notified" do 
-    expect {  Train.look_for_service_status! }.to change { ServiceOutage.count }.by(1)
+  it "creates a service outage" do 
+    t = Train.create
+    expect { t.create_current_outage }.to change { ServiceOutage.count }.by(1) 
+  end
+
+  it "updates the end time of the current outage" do 
+    t = Train.create
+    t.create_current_outage
+    t.end_current_outage
+    expect { t.current_outage.outage_end }.to_not change { ServiceOutage.count }
   end
 end
