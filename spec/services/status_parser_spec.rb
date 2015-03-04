661023ca6cb3dcd66b5acab3b20cf3249a7619e9
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2015-03-04 14:41:19 -0500

    prototype built for status parser

--- spec/services/status_parser_spec.rb
+++ spec/services/status_parser_spec.rb
@@ -1,16 +1,43 @@
-def find_trains!(string) 
-  chars = string.split("")
-  trains = []
-  chars.each_with_index do |char, i|
-    trains << chars[i+1].to_i if char == '['
+class StatusParser
+  def self.find_start_time!(string)
+    string.scan(/\d{1,2}:\d{2}../).join  
   end
-  trains
+  
+  
+  def self.find_trains!(string) 
+    chars = string.split("")
+    trains = []
+    chars.each_with_index do |char, i|
+      trains << chars[i+1] if char == '['
+    end
+    trains.uniq
+  end
 end
 
 RSpec.describe "StatusIdentifier" do 
-  it "correctly cleans away any markup" do 
-    string = "<span class='TitleDelay'>Delays</span>                    <span class='DateStyle'>                    &nbsp;Posted:&nbsp;03/02/2015&nbsp; 2:03PM                    </span><br/><br/>                  <P>Due to track maintenance at <STRONG>149 St-Grand Concourse</STRONG>, southbound [2] and [5] trains are running with delays.</P><P>Allow additional travel time.</P>                <br/><br/>"
-    trains = find_trains!(string)
-    expect(trains).to eq([2, 5])
+  describe "#StatusParser.find_trains!" do 
+    it "correctly identifies multiple trains with delays" do 
+      string = "<span class='TitleDelay'>Delays</span>                    <span class='DateStyle'>                    &nbsp;Posted:&nbsp;03/02/2015&nbsp; 2:03PM                    </span><br/><br/>                  <P>Due to track maintenance at <STRONG>149 St-Grand Concourse</STRONG>, southbound [2] and [5] trains are running with delays.</P><P>Allow additional travel time.</P>                <br/><br/>"
+      trains = StatusParser.find_trains!(string)
+      expect(trains).to eq ["2", "5"]
+    end
+
+    it "correctly identifies a single train delay" do 
+      string = "                    <span class='TitleServiceChange' >Service Change</span>                    <span class='DateStyle'>                    &nbsp;Posted:&nbsp;03/02/2015&nbsp; 8:01PM                    </span><br/><br/>                  <P>Due to a rail condition at <STRONG>Grant Av</STRONG>, the following service changes are in effect: </P><P>Southbound [A] trains are running with delays.</P><P>Some southbound [A] trains terminate at<STRONG> Euclid Av</STRONG>. </P><P>Allow additional travel time.</P>                <br/><br/>              "
+      trains = StatusParser.find_trains!(string)
+      expect(trains).to eq ["A"]
+    end
+  end
+
+  describe '#StatusParser.find_start_time!' do 
+    it "correctly finds a time with a leading single digit" do 
+      string = "<span class='TitleDelay'>Delays</span>                    <span class='DateStyle'>                    &nbsp;Posted:&nbsp;03/02/2015&nbsp; 2:03PM                    </span><br/><br/>                  <P>Due to track maintenance at <STRONG>149 St-Grand Concourse</STRONG>, southbound [2] and [5] trains are running with delays.</P><P>Allow additional travel time.</P>                <br/><br/>"
+      expect(StatusParser.find_start_time!(string)).to eq("2:03PM")
+    end
+
+    it "correctly finds a start time with two leading digits" do 
+      string = "                    <span class='TitleServiceChange' >Service Change</span>                    <span class='DateStyle'>                    &nbsp;Posted:&nbsp;03/02/2015&nbsp; 10:01AM                    </span><br/><br/>                  <P>Due to a rail condition at <STRONG>Grant Av</STRONG>, the following service changes are in effect: </P><P>Southbound [A] trains are running with delays.</P><P>Some southbound [A] trains terminate at<STRONG> Euclid Av</STRONG>. </P><P>Allow additional travel time.</P>                <br/><br/>              "
+      expect(StatusParser.find_start_time!(string)).to eq("10:01AM")
+    end
   end
 end
