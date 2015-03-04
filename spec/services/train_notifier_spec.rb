f16cd52a5f8474d143611ce3fb3930a92d56bad6
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2015-03-04 14:58:43 -0500

    add train notifier specs

--- spec/services/train_notifier_spec.rb
+++ spec/services/train_notifier_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe "TrainNotifier" do
     allow(j).to receive(:parse) { {"responsecode"=>"0", "timestamp"=>"3/1/2015 11:10:00 AM", "subway"=> {"line"=>[{"name"=>"123", "status"=>"DELAYS", "text"=>{}, "Date"=>{}, "Time"=>{}}]}} }
     t = double('Train', :name => '123', :current_outage => false)
     allow(t).to receive(:set_current_outage=)
+    allow(t).to receive(:save)
     notifier = TrainNotifier.new(j)
     notifier.trains << t
     notifier.evaluate! 
@@ -19,9 +20,20 @@ RSpec.describe "TrainNotifier" do
     allow(j).to receive(:parse) { {"responsecode"=>"0", "timestamp"=>"3/1/2015 11:10:00 AM", "subway"=> {"line"=>[{"name"=>"123", "status"=>"GOOD SERVICE", "text"=>{}, "Date"=>{}, "Time"=>{}}]}} }
     t = double('Train', :name => '123', :current_outage => true)
     allow(t).to receive(:set_current_outage=)
+    allow(t).to receive(:save)
     notifier = TrainNotifier.new(j)
     notifier.trains << t
     notifier.evaluate! 
     expect(t).to have_received(:set_current_outage=).with(false)
+  end
+
+  it "changes a train's status if specified" do 
+    t = double('Train', :current_outage => false)  
+    j = instance_double("JSONParser")
+    allow(j).to receive(:parse)
+    allow(t).to receive(:set_current_outage=)
+    allow(t).to receive(:save)
+    notifier = TrainNotifier.new(j).change_train_status(t)
+    expect(t).to have_received(:set_current_outage=).with(true)
   end
 end
