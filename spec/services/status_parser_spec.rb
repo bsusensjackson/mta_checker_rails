4480603b139c29733753eebe874810996fa5aeb4
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2015-03-04 14:58:19 -0500

    add specs for status parser

--- spec/services/status_parser_spec.rb
+++ spec/services/status_parser_spec.rb
@@ -1,18 +1,4 @@
-class StatusParser
-  def self.find_start_time!(string)
-    string.scan(/\d{1,2}:\d{2}../).join  
-  end
-  
-  
-  def self.find_trains!(string) 
-    chars = string.split("")
-    trains = []
-    chars.each_with_index do |char, i|
-      trains << chars[i+1] if char == '['
-    end
-    trains.uniq
-  end
-end
+require 'rails_helper'
 
 RSpec.describe "StatusIdentifier" do 
   describe "#StatusParser.find_trains!" do 
