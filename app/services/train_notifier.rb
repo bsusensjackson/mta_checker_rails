6aa10814b6ca18102d262576d2e501416ac9d2d9
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2015-03-04 14:59:07 -0500

    add train_notifier

--- app/services/train_notifier.rb
+++ app/services/train_notifier.rb
@@ -1,5 +1,7 @@
 class TrainNotifier
   attr_accessor :trains
+  attr_reader :status_hash
+  
   def initialize(json_parser)
     @status_hash = json_parser.parse
     @trains = Train.all
@@ -10,12 +12,18 @@ class TrainNotifier
       @trains.each do |train|
         if train.name == item['name'] 
           if item['status'] == 'DELAYS' && train.current_outage == false
-            train.set_current_outage = true
+            change_train_status(train)
           elsif item['status'] == 'GOOD SERVICE' && train.current_outage == true
-            train.set_current_outage = false
+            change_train_status(train)
           end
         end
       end 
     end 
+  end
+
+  def change_train_status(train)
+    current = train.current_outage
+    train.set_current_outage = !current
+    train.save
   end
 end
