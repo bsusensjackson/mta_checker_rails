61ea997fe4e8d13fe95b1b51866edc57878a92ce
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2015-03-09 21:07:27 -0700

    initial logic fleshed out for train notifier

--- app/services/train_notifier.rb
+++ app/services/train_notifier.rb
@@ -1,29 +1,51 @@
 class TrainNotifier
-  attr_accessor :trains
-  attr_reader :status_hash
-  
-  def initialize(json_parser)
+  OUTAGES = {
+    "GOOD SERVICE" => false,
+    "DELAYS" => true
+  }
+
+  def initialize(json_parser, status_parser)
     @status_hash = json_parser.parse
-    @trains = Train.all
+    @status_parser = status_parser
   end
 
   def evaluate! #TODO- Refactor Me
-    @status_hash['subway']['line'].each do |item|
-      @trains.each do |train|
-        if train.name == item['name'] 
-          if item['status'] == 'DELAYS' && train.current_outage == false
-            change_train_status(train)
-          elsif item['status'] == 'GOOD SERVICE' && train.current_outage == true
-            change_train_status(train)
-          end
-        end
-      end 
-    end 
+
+  #TRAINs with current outage - 
+    #IF TRAIN is no longer delayed
+    #CHANGE TRAIN STATUS to !delayed
+  #TRAINS with no outage
+    #IF TRAIN is delayed
+    #CHANGE TRAIN STATUS to delayed
+  
+    @status_hash['subway']['line'].each do |line|
+      trains = break_up_text(line['name'])
+      
+      trains.each do |line| 
+        t = find_train(line)
+        notify_train(t) { t.outage? == OUTAGES[line['status']] }
+      end
+    end
+  end
+
+  private 
+
+  attr_reader :status_parser, :status_hash
+ 
+  def find_train(line)
+    Train.first_or_create(name: line)
+  end
+
+  def notify_train(train, &block)
+    change_train_status train if yield
   end
 
   def change_train_status(train)
-    current = train.current_outage
-    train.set_current_outage = !current
+    train.set_current_outage(!train.outage?)
     train.save
   end
+
+  def break_up_text(string)
+    ar = string.split("")
+  end 
 end
